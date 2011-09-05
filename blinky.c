a9473417912a1aa81155ce0de6088eef497aa97d
Jithu Sunny <jithusunnyk@gmail.com>
2011-09-05 22:01:55 +0530

    added some missing C syntax elements..;)

--- blinky.c
+++ blinky.c
@@ -3,11 +3,13 @@
 
 int main()
 {
-	DDRC = 0xff;
+	int i;
+        DDRC = 0xff;
 	
 	while (1)
 	        for(i = 0; i < 8; i++) {
 	                PORTC = i;
 	       		_delay_ms(1000); 
 		}
+	return 0;
 }
